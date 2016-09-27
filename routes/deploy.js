70af8d3ed9bb576651493556d86b8399d044ea9c
Anil Dukkipatty <anildukkipatty@gmail.com>
2016-09-27 18:03:19 +0530

    messages added

--- routes/deploy.js
+++ routes/deploy.js
@@ -11,18 +11,18 @@ var exec = require('child_process').exec;
 
 router.post('/:id/callback', function(req, res) {
   var data = req.body;
-  if (! data.ref) return res.sendStatus(500);
+  if (! data.ref) return res.send('not a request', 500);
   // return res.sendStatus(200);
 
   Project.findOne({_id: mongoose.Types.ObjectId(req.params.id)}, function (err, project) {
-    if (err || !project) return res.sendStatus(500);
+    if (err || !project) return res.send('project not found', 500);
     // return output
 
     var slots = _.filter(project.slots, {ref: data.ref});
-    if (slots.length <= 0) return res.sendStatus(500);
+    if (slots.length <= 0) return res.send('No slot found', 500);
     var slot = slots[0];
 
-    if (! slot.status) return res.send(500);
+    if (! slot.status) return res.send('Auto deploy disabled', 500);
 
     var connectionString = "ssh " + slot.serverUser + "@" + slot.server + " ";
     var commands = slot.commands;
