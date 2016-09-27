2f806fcf67fb0ff92f3cdaf58c61f7bc8cb613d8
Anil Dukkipatty <anildukkipatty@gmail.com>
2016-09-27 20:20:50 +0530

    cleanup

--- routes/deploy.js
+++ routes/deploy.js
@@ -12,43 +12,30 @@ var exec = require('child_process').exec;
 router.post('/:id/callback', function(req, res) {
   var data = req.body;
   if (! data.ref) return res.send('not a request', 500);
-  // return res.sendStatus(200);
 
   Project.findOne({_id: mongoose.Types.ObjectId(req.params.id)}, function (err, project) {
     if (err || !project) return res.send('project not found', 500);
-    // return output
 
     var slots = _.filter(project.slots, {ref: data.ref});
     if (slots.length <= 0) return res.send('No slot found', 500);
     var slot = slots[0];
-
     if (! slot.status) return res.send('Auto deploy disabled', 500);
-
 
     var connectionString = "ssh " + project.serverUser + "@" + project.server + " ";
     var commands = "cd " + slot.location + " && " + slot.commands;
-    console.log(connectionString + commands);
 
     exec('ssh-keyscan -H -p 22 '+ project.server +' >> ~/.ssh/known_hosts',
       function (err, stdOut, stdErr) {
-        console.log('keyscan out: ' + stdOut);
-        console.log('keyscan err: ' + stdErr);
-        if (err) console.log(err);
-
 
         exec(connectionString + commands,
           function (err, stdOut, stdErr) {
-            console.log('out: ' + stdOut);
-            console.log('err: ' + stdErr);
-
             if (err) console.log(err);
-
             return res.send(stdOut);
           }
-        )
+        );
 
       }
-    )
+    );
 
 
   });
