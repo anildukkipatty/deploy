17c7243ed5955b00d382fa2fba774d477e968651
Anil Dukkipatty <anildukkipatty@gmail.com>
2016-10-04 10:07:25 +0530

    added manual deploy WIP

--- routes/deploy.js
+++ routes/deploy.js
@@ -53,5 +53,47 @@ router.post('/:id/callback', function(req, res) {
   });
 });
 
+router.post('/:id/manual', function(req, res) {
+  var data = req.body;
+
+  Project.findOne({_id: mongoose.Types.ObjectId(req.params.id)}, function (err, project) {
+    if (err || !project) return res.send('project not found', 500);
+
+    var slots = _.filter(project.slots, {name: data.name});
+    if (slots.length <= 0) return res.send('No slot found', 500);
+    var slot = slots[0];
+    if (! slot.status) return res.send('Auto deploy disabled', 500);
+
+    var deployResult = {stage: 1};
+    var connectionString = "ssh " + project.serverUser + "@" + project.server + " ";
+    var commands = "cd " + slot.location + " && " + slot.commands;
+
+    exec('ssh-keyscan -H -p 22 '+ project.server +' >> ~/.ssh/known_hosts',
+      function (err, stdOut, stdErr) {
+
+        exec(connectionString + "'" + commands + "'",
+          function (err, stdOut, stdErr) {
+            deployResult.stage = 2;
+            deployResult.stdout = stdOut;
+            deployResult.stderr = stdErr;
+            if (err) {
+              deployResult.err = err;
+              deployResult.stage = 3;
+            };
+
+            var slotIndex = project.slots.indexOf(slot);
+            project.slots[slotIndex].results.push(deployResult)
+            project.save(function (data) {
+              return res.send(stdOut);
+            });
+          }
+        );
+
+      }
+    );
+
+
+  });
+});
 
 module.exports = router;
